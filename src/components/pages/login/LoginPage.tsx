06c54cbf60a524608b6091c73625e4819823de30
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-01-21 20:20:14 +0100

    ✨ Login : connect backend and frontend

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,22 +1,73 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
+import { yupSchema } from "./yupSchema";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
+type FormValues = {
+  username: string;
+  email: string;
+  password: string;
+};
 export default function LoginPage() {
-  const { register, handleSubmit, formState : {errors} } = useForm({resolver: yupResolver(yupSchema)});
+  const navigate = useNavigate()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver: yupResolver(yupSchema) });
   const [isLogin, setIsLogin] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const onSubmit = async (data: FormValues) => {
+    const url = isLogin
+    ? "http://localhost:8080/api/user/login"
+    : "http://localhost:8080/api/user";
+
+  try {
+    const response = await axios.post(url, data);
+
+    if (!isLogin) {
+      alert("Inscription réussie !");
+    }
+    navigate("/oezihfozfe");
+  } catch (error: any) {
+    if (error.response) {
+      setErrorMessage(error.response.data.message || "Une erreur est survenue");
+    } else {
+      setErrorMessage("Erreur de connexion au serveur");
+    }
+  }
+};
+
   return (
-    <FormStyled>
+    <FormStyled onSubmit={handleSubmit(onSubmit)}>
       <h1>{isLogin ? "Connexion" : "Inscription"}</h1>
 
-      {!isLogin && <input placeholder="nom" />}
-      <input placeholder="email" />
-      <input placeholder="mot de passe" />
+      {!isLogin && (
+        <>
+          <input placeholder="nom" {...register("username")} type="username" />{" "}
+          {errors.username && <p>{errors.username.message}</p>}{" "}
+        </>
+      )}
+
+      <input placeholder="email" {...register("email")} type="email" />
+      {errors.email && <p>{errors.email.message}</p>}
+      <input
+        placeholder="mot de passe"
+        {...register("password")}
+        type="password"
+      />
+      {errors.password && <p>{errors.password.message}</p>}
 
       <button type="submit">{isLogin ? "Se connecter" : "S'inscrire"}</button>
 
       <span onClick={() => setIsLogin(!isLogin)}>
-        {isLogin ? "Pas encore de compte ? S'inscrire" : "Déjà un compte ? Se connecter"}
+        {isLogin
+          ? "Pas encore de compte ? S'inscrire"
+          : "Déjà un compte ? Se connecter"}
       </span>
     </FormStyled>
   );
@@ -28,28 +79,32 @@ const FormStyled = styled.form`
   justify-content: center;
   align-items: center;
   height: 100vh;
-  gap: 20px;
-  background-color: #F9F9F9;
+  background-color: #f9f9f9;
 
-  button{
+  button {
+    margin-top: 20px;
     padding: 12px;
     border-radius: 20px;
     width: 300px;
-    background-color: #1E90FF;
+    background-color: #1e90ff;
     color: white;
   }
 
-  input{
+  input {
     padding: 12px;
-    border-radius: 20px; 
+    border-radius: 20px;
     width: 300px;
+    margin-top: 20px;
   }
-  span{
-    color: #1E90FF;
+  span {
+    color: #1e90ff;
     cursor: pointer;
-    &:hover{
+    &:hover {
       text-decoration: underline;
       color: #0069d2;
     }
   }
-`
+  p{
+    color: #FF4D4D;
+  }
+`;
