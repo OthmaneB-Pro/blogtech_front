1f8d37dd3154acef5e4073d1a64d395d30ef4c6a
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-02-25 12:13:20 +0100

    💄 Home : add style and grid template

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -1,37 +1,96 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 export default function HomePage() {
+  const navigate = useNavigate();
   return (
     <div>
+      
       <div>Article :</div>
 
-      <CardStyled>
-        <h1>Title</h1>
-        <p>Content</p>
-        <span>Author</span>
-        <span>Dernière maj</span>
-      </CardStyled>
+      <ArticleStyled>
+        <CardStyled>
+          <h1>Title</h1>
+          <p>Content</p>
+          <div className="info">
+            <span>Author</span>
+            <span>Dernière maj</span>
+          </div>
+        </CardStyled>
+        <CardStyled>
+          <h1>Title</h1>
+          <p>Content</p>
+          <div className="info">
+            <span>Author</span>
+            <span>Dernière maj</span>
+          </div>
+        </CardStyled>
+        <CardStyled>
+          <h1>Title</h1>
+          <p>Content</p>
+          <div className="info">
+            <span>Author</span>
+            <span>Dernière maj</span>
+          </div>
+        </CardStyled>
+        <CardStyled>
+          <h1>Title</h1>
+          <p>Content</p>
+          <div className="info">
+            <span>Author</span>
+            <span>Dernière maj</span>
+          </div>
+        </CardStyled>
+        <CardStyled>
+          <h1>Title</h1>
+          <p>Content</p>
+          <div className="info">
+            <span>Author</span>
+            <span>Dernière maj</span>
+          </div>
+        </CardStyled>
+        <CardStyled>
+          <h1>Title</h1>
+          <p>Content</p>
+          <div className="info">
+            <span>Author</span>
+            <span>Dernière maj</span>
+          </div>
+        </CardStyled>
+      </ArticleStyled>
     </div>
   );
 }
 
 const CardStyled = styled.div`
-  display : flex;
+  display: flex;
   flex-direction: column;
   justify-content: center;
   // background: grey;
   width: 300px;
   border: 1px solid black;
   border-radius: 5px;
-  box-shadow: 10px 5px 5px rgba(63, 63, 63, 0.5);
-  
-  h1{
+  box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+  h1 {
     background-color: green;
   }
-  p{
+  p {
     padding: 15px;
     height: 130px;
     margin-top: 0px;
     background-color: blue;
   }
-  `
+  .info {
+    background-color: red;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+  }
+`;
+
+const ArticleStyled = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  row-gap: 30px;
+
+`
