b5e9a9b96688e3534104c93899bbe5a36518f7af
OthmaneB-Pro <othmane93.baar@gmail.com>
2025-03-02 17:02:29 +0100

    ♻️ Home : Refacto Card Values

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import Card from "../../reusable-ui/Card";
+import { cardValues } from "./cardValues";
 
 export default function HomePage() {
   const navigate = useNavigate();
@@ -11,84 +13,13 @@ export default function HomePage() {
       <div>Article :</div>
 
       <ArticleStyled>
-        <CardStyled>
-          <h1>Title</h1>
-          <p>Content</p>
-          <div className="info">
-            <span>Author</span>
-            <span>Dernière maj</span>
-          </div>
-        </CardStyled>
-        <CardStyled>
-          <h1>Title</h1>
-          <p>Content</p>
-          <div className="info">
-            <span>Author</span>
-            <span>Dernière maj</span>
-          </div>
-        </CardStyled>
-        <CardStyled>
-          <h1>Title</h1>
-          <p>Content</p>
-          <div className="info">
-            <span>Author</span>
-            <span>Dernière maj</span>
-          </div>
-        </CardStyled>
-        <CardStyled>
-          <h1>Title</h1>
-          <p>Content</p>
-          <div className="info">
-            <span>Author</span>
-            <span>Dernière maj</span>
-          </div>
-        </CardStyled>
-        <CardStyled>
-          <h1>Title</h1>
-          <p>Content</p>
-          <div className="info">
-            <span>Author</span>
-            <span>Dernière maj</span>
-          </div>
-        </CardStyled>
-        <CardStyled>
-          <h1>Title</h1>
-          <p>Content</p>
-          <div className="info">
-            <span>Author</span>
-            <span>Dernière maj</span>
-          </div>
-        </CardStyled>
+        {cardValues.map((card) => (
+          <Card key={card.date} title={card.title} content={card.content} author={card.author} date={card.date} />
+        ))}
       </ArticleStyled>
     </div>
   );
 }
-
-const CardStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  // background: grey;
-  width: 300px;
-  border: 1px solid black;
-  border-radius: 5px;
-  box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
-  h1 {
-    background-color: green;
-  }
-  p {
-    padding: 15px;
-    height: 130px;
-    margin-top: 0px;
-    background-color: blue;
-  }
-  .info {
-    background-color: red;
-    display: flex;
-    justify-content: space-between;
-    padding: 10px;
-  }
-`;
 
 const ArticleStyled = styled.div`
   display: grid;
